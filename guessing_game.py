b3139d11859ad8fa4df152f4b9665a2910c176f3
Levis155 <levismbui@gmail.com>
2025-07-28 11:22:53 +0300

    add exercise for conditionals

---
+++ guessing_game.py
@@ -0,0 +1,10 @@
+myFavoriteFood = "chicken"
+
+guess = input("What's my favorite food?")
+
+if guess == myFavoriteFood:
+    print("Yep! So amazing!")
+else:
+    print("Yuck! That's not it!")
+    
+print("Thanks for playing!")
