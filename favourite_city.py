18b88857a808913a3f01a85aebdcd74b9984e0d3
Levis155 <levismbui@gmail.com>
2025-07-29 07:12:58 +0300

    add function exercise

---
+++ favourite_city.py
@@ -0,0 +1,6 @@
+def say_favorite_city(city_name):
+    print("One of my favorite cities is", city_name)
+    
+say_favorite_city("Nairobi")
+say_favorite_city("Barcelona")
+say_favorite_city("New York")
