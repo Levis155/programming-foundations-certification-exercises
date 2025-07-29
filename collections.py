d0c76b13c2b06a5d854a1f9ac716080dcabc8325
Levis155 <levismbui@gmail.com>
2025-07-29 11:24:55 +0300

    add exercise on collections

---
+++ collections.py
@@ -0,0 +1,20 @@
+stars = [
+    "sol",
+    "Alpha Centauri",
+    "Barnard",
+    "Wolf 359"
+]
+
+print(stars[3])
+
+peaks = {
+    "African": "Kilimanjaro",
+    "Antarctic": "Vinson",
+    "Australian": "Puncak Jaya",
+    "Eurasian": "Everest",
+    "North American": "Denali",
+    "Pacific": "Mauna Kea",
+    "South American": "Aconcagua"
+}
+
+print(peaks["Pacific"])
